88211d73c235b615ef0fdce733d18ddd57752d58
maximmnzh <maxim.mnzh@gmail.com>
2022-03-03 16:34:13 +0300

    Реализован выбор языка интерфейса браузера

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    time.sleep(30)
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
